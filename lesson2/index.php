43e94192667e16659b86aa9e8cf7022c59a877dd
erisk1 <eriskarabeg@gmail.com>
2025-03-13 08:13:12 -0700

    lesson2

---
+++ lesson2/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $school = 'digitalschool';
+    echo "i love $school" . "<br>";
+
+    $x = 120;
+    $y = 50;
+
+    echo $x + $y . "<br>";
+    echo $x - $y . "<br>";
+    echo $x / $y . "<br>";
+    echo $x * $y . "<br>";
+    echo $x % $y . "<br>";
+
+    $a = 'digital';
+    $b = 'school';
+
+    $c = $a . $b;
+    echo = "$a.$b/n";
+
+
+    ?>
+</body>
+</html>
