962240c637743e346aab41175d290cbab3c17bb2
erisk1 <eriskarabeg@gmail.com>
2025-03-25 16:18:18 +0100

    lesson4

---
+++ lesson4/index.php
@@ -0,0 +1,72 @@
+<?php 
+    // phpinfo();
+
+   
+    
+    // $x = 5;
+    // echo gettype($x) . "<br>";
+    // double 10.5
+
+    $x = 10.5;
+    echo gettype($x) . "<br>";
+    // string
+
+    $x = "hello";
+    echo gettype($x) . "<br>";
+?>
+<?php
+//     function displayPhpVersion(){
+//         echo "this is PHP" .phpversion();
+//         echo "\n";
+//     }
+
+//     displayPhpVersion();
+ ?>
+
+ <?php
+
+    //   function hello(){
+    //     echo "hello world";
+    //   }
+
+    //   hello();
+ ?>
+ <?php 
+// function sum(){
+//     $value = 120+20;
+//     echo $value;
+// }
+// sum();
+ ?>
+
+ <?php
+    //   function maximum($x, $y){
+    //     if ($x > $y){
+    //         return $x;
+    //     }
+    //     else{
+    //         return $y;
+    //     }
+    //   }
+
+    //   $a = 20;
+    //   $b = 30;
+    
+    //   $test = "the max of $a and $b is $test";
+?>
+
+<?php 
+    function divisible($n){
+        if(($n % 2) == 0){
+            return"$n eshte e plotpjestueshme me 2";
+        }
+        else{
+            return"$n nuk eshte e plotpjestueshme me 2";
+        }
+    }
+
+    print_r(divisible(4). "<br>");
+    print_r(divisible(35). "<br>");
+    print_r(divisible(16). "<br>");
+    print_r(divisible(3). "<br>");
+?>
