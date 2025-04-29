be8e289b4a0f96af34b6e9bcf80fc00e073848d9
erisk1 <eriskarabeg@gmail.com>
2025-04-29 15:46:11 +0200

    CHANGES

---
+++ lesson8/insertdate.php
@@ -0,0 +1,35 @@
+ <?php
+// try{
+// $pdo = new PDO ("mysql:host=localhost;dbname=db" , "root", "");
+
+// $username = "jack";
+
+// $password = password_hash("mypassword" , PASSWORD_DEFAULT);
+
+// $sql = "INSERT INTO users (username,password) VALUES ('$username', '$password')";
+
+// $pdo -> exec($sql);
+
+// echo "new record created successfully." . $last_id;
+
+// }
+// catch (PDOException $e){
+
+//     echo $e -> getMessage();
+// }
+
+// try{
+//     $pdo = new PDO ("mysql:host=localhost;dbname=db", "root","");
+
+//     $sql = "ALTER TABLE products DROP COLUMN name";
+
+//     $pdo -> exec($sql):
+//     echo "Column droped succesfully";
+
+// }
+
+// catch(PDOException $e){
+//     echo $e -> getMessage();
+// }
+// 
+?> 
